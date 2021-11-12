cc34abe35d7064907166cd4dd9860c3402d37329
Krisztián Tolnai <krisztian.tolnai@stud.uni-corvinus.hu>
2021-11-12 16:19:04 +0100

    edited Ball class

--- UserMaintenance/week08/Entities/Ball.cs
+++ UserMaintenance/week08/Entities/Ball.cs
@@ -5,31 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Abstractions;
 
 namespace week08.Entities
 {
-    public class Ball: Label
+    public class Ball: Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Width = 50;
-            Height = Width;
-            Paint += Ball_Paint;
-    }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics picture)
+        protected override void DrawImage(Graphics picture)
         {
             picture.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
-        public void MoveBall()
-        {
-            Left += 1;
-        }
+
     }
 }
