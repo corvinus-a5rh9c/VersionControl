ada6c62d002e0e2b606cf98e9eb7b4b443287dd6
Krisztián Tolnai <krisztian.tolnai@stud.uni-corvinus.hu>
2021-11-12 16:26:12 +0100

    added car class

---
+++ UserMaintenance/week08/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserMaintenance.Abstractions;
+
+namespace week08.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics picture)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            picture.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
