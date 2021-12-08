9d647931eb26732203f1e8b0be563ad5e880ebae
Krisztián Tolnai <krisztian.tolnai@stud.uni-corvinus.hu>
2021-12-08 09:35:09 +0100

    Finished project

--- UserMaintenance/week12/Form1.cs
+++ UserMaintenance/week12/Form1.cs
@@ -79,10 +79,20 @@ namespace week12
                 {
                     winnerBrain = winners.FirstOrDefault().Brain.Clone();
                     gc.GameOver -= Gc_GameOver;
+                    button1.Visible = true;
                     return;
                 }
             }
             gc.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
         }
     }
 }
