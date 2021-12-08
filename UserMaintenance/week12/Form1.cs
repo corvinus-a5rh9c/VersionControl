e3f3966359247c0384dd57897dfe47c869467679
Krisztián Tolnai <krisztian.tolnai@stud.uni-corvinus.hu>
2021-12-08 09:27:23 +0100

    Added new generation

--- UserMaintenance/week12/Form1.cs
+++ UserMaintenance/week12/Form1.cs
@@ -16,6 +16,8 @@ namespace week12
         GameController gc = new GameController();
         GameArea ga;
 
+
+
         int populationSize = 100;
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
@@ -37,11 +39,9 @@ namespace week12
             gc.GameOver += Gc_GameOver;
             gc.Start();
 
-            var playerList = from p in gc.GetCurrentPlayers()
-                             orderby p.GetFitness() descending
-                             select p;
-            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
         }
+
 
         private void Gc_GameOver(object sender)
         {
@@ -49,6 +49,30 @@ namespace week12
             label1.Text = string.Format(
                 "{0}. generáció",
                 generation);
+
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
